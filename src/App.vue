e848dd8f44b300d40383eb5db0986fe5646bc351
Mateus Silva <mateus42silva@gmail.com>
2023-07-16 19:46:39 -0300

    create navbar and footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <NavBar />
+  <div>
+    <NavBar :logo="logo_src" :alt="app_name"/>
   <router-view/>
   <Footer />
+  </div>
+  
 </template>
 
 <script>
@@ -13,10 +16,34 @@
     components:{
       NavBar,
       Footer
+    },
+    data(){
+      return {
+        logo_src: '/img/logo.png',
+        app_name: 'Make Your Burger'
+      }
     }
   }
 </script>
 
 <style>
+*{
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0;
+  margin:0;
+  box-sizing: border-box;
 
+}
+
+.main-container{
+  margin:50px;
+  min-height: 250px;
+}
+
+h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <p>teste</p>
-    </div>
+    <footer>
+            <p>Make Your Burger &copy; 2023</p>
+    </footer>
 </template>
 
 <script>
@@ -9,3 +9,17 @@
         name:'Footer'
     }
 </script>
+
+<style scoped>
+    footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #fcba03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
