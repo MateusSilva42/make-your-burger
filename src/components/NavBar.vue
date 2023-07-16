4411414bd3ca33b0aa128b259683749c7e65fa1b
Mateus Silva <mateus42silva@gmail.com>
2023-07-16 18:48:15 -0300

    config routes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <router-link to="/">
+        <img src="../../public/img/logo.png" id="logo">
+    </router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Pedidos">Pedidos</router-link>
+  </nav>
+</template>
+
+<script>
+export default{
+    name:'NavBar'
+}
+
+</script>
+
+<style>
+
+</style>
